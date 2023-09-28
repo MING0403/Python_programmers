82c97171b1c167ada5385312ff45a5197bca4c02
Minah Han <mi.in.ah0403@gmail.com>
2023-09-28 17:29:11 +0900

    [level 0] Title: OX퀴즈, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,10 @@
+def solution(quiz):
+    answer = []
+    for i in quiz:
+        # 
+        if eval(i.replace("=","==")):
+            answer.append("O")
+        else:
+            answer.append("X")
+
+    return answer
